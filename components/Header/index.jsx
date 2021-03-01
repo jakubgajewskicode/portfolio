fbf9840657eec48fc9d115fddd6c35fb17d0bb1b
Jakub Gajewski <jakub.gajewski@yahoo.com>
2021-03-01 19:42:00 +0000

    upgrade header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,25 +2,26 @@ import Link from 'next/link';
 
 import { Header, H3, StyledHamburger, Hamburger, TopBar, BottomBar } from './ui';
 
+// eslint-disable-next-line react/prop-types
 const MainHeader = ({ barState, setBarState }) => {
-	const Toogle = () => {
-		setBarState((state) => !state);
-	};
+  const Toogle = () => {
+    setBarState((state) => !state);
+  };
 
-	return (
-		<Header>
-			<H3 colorTitle={barState}>
-				<Link href='/'>Jakub Gajewski</Link>
-			</H3>
+  return (
+    <Header>
+      <H3 colorTitle={barState}>
+        <Link href="/">Jakub Gajewski</Link>
+      </H3>
 
-			<StyledHamburger>
-				<Hamburger onClick={Toogle}>
-					<TopBar topChanged={barState} />
-					<BottomBar bottomChanged={barState} />
-				</Hamburger>
-			</StyledHamburger>
-		</Header>
-	);
+      <StyledHamburger>
+        <Hamburger onClick={Toogle}>
+          <TopBar topChanged={barState} />
+          <BottomBar bottomChanged={barState} />
+        </Hamburger>
+      </StyledHamburger>
+    </Header>
+  );
 };
 
 export default MainHeader;
--- components/Header/ui.jsx
+++ components/Header/ui.jsx
@@ -1,57 +1,59 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const Header = styled.header`
-	display: flex;
-	justify-content: space-between;
-	margin-bottom: 20px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
 
-	height: 100px;
-	align-items: center;
+  height: 100px;
+  align-items: center;
 `;
 
 export const H3 = styled.h3`
-	font-weight: 200;
-	z-index: 2;
-	margin: 0;
+  font-weight: 200;
+  z-index: 2;
+  margin: 0;
 
-	a {
-		text-decoration: none;
-		color: ${({ colorTitle }) => (colorTitle === true ? `#ffffff` : `#000000`)};
-		transition: color 400ms cubic-bezier(0.8, 0, 0.55, 0.94) 400ms;
-	}
+  a {
+    text-decoration: none;
+    color: ${({ colorTitle }) => (colorTitle === true ? `#ffffff` : `#000000`)};
+    transition: color 400ms cubic-bezier(0.8, 0, 0.55, 0.94) 400ms;
+  }
 `;
 
 export const Hamburger = styled.div`
-	width: 25px;
-	height: 25px;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	cursor: pointer;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
 
-	z-index: 10;
+  z-index: 10;
 `;
 
 export const TopBar = styled.div`
-	width: 100%;
-	height: 1px;
-	z-index: 10;
-	background-color: ${({ topChanged }) => (topChanged === true ? `#ffffff;` : `#000;`)};
-	transition: transform 400ms cubic-bezier(0.8, 0, 0.55, 0.94);
-	transform-origin: 100% 0;
-	transform: ${({ topChanged }) => (topChanged === true ? `translate3d(-5px, 3px, 0) rotate(-45deg)` : `translate3d(0, 10px, 0)`)};
+  width: 100%;
+  height: 1px;
+  z-index: 10;
+  background-color: ${({ topChanged }) => (topChanged === true ? `#ffffff;` : `#000;`)};
+  transition: transform 400ms cubic-bezier(0.8, 0, 0.55, 0.94);
+  transform-origin: 100% 0;
+  transform: ${({ topChanged }) =>
+    topChanged === true ? `translate3d(-5px, 3px, 0) rotate(-45deg)` : `translate3d(0, 10px, 0)`};
 `;
 
 export const BottomBar = styled.div`
-	width: 100%;
-	height: 1px;
-	z-index: 10;
-	background-color: ${({ bottomChanged }) => (bottomChanged === true ? `#ffffff;` : `#000;`)};
-	transition: transform 400ms cubic-bezier(0.8, 0, 0.55, 0.94);
-	transform-origin: 100% 100%;
-	transform: ${({ bottomChanged }) => (bottomChanged === true ? `translate3d(-5px, -3px, 0) rotate(45deg)` : `translate3d(0, 0px, 0)`)};
+  width: 100%;
+  height: 1px;
+  z-index: 10;
+  background-color: ${({ bottomChanged }) => (bottomChanged === true ? `#ffffff;` : `#000;`)};
+  transition: transform 400ms cubic-bezier(0.8, 0, 0.55, 0.94);
+  transform-origin: 100% 100%;
+  transform: ${({ bottomChanged }) =>
+    bottomChanged === true ? `translate3d(-5px, -3px, 0) rotate(45deg)` : `translate3d(0, 0px, 0)`};
 `;
 
 export const StyledHamburger = styled.div`
-	z-index: 2;
+  z-index: 2;
 `;
