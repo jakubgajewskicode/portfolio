7934409508c23dfd76f6a6860315be412bb0776e
Jakub Gajewski <jakub.gajewski@yahoo.com>
2021-03-02 07:52:40 +0000

    update _document

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,24 @@
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+          // useful for wrapping the whole react tree
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(
+              <>
+                <App {...props} />
+              </>
+            ),
+          // useful for wrapping in a per-page basis
+          enhanceComponent: (Component) => Component
         });
-
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -26,4 +33,16 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+export default MyDocument;
