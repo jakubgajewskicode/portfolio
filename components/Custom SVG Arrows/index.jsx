c139b438a391f33b2450a7f3b06361f101aeb0c8
Jakub Gajewski <jakub.gajewski@yahoo.com>
2021-05-11 20:26:03 +0100

    rem description

--- components/Custom SVG Arrows/index.jsx
+++ components/Custom SVG Arrows/index.jsx
@@ -3,10 +3,7 @@ import { SVGWrap, RightSVGArrows } from './ui';
 const SVGArrows = () => {
   return (
     <>
-      <div>
-        <p>Creating 3 vector triangles in Figma and manipulating it in css</p>
-        <SVGWrap>{RightSVGArrows}</SVGWrap>
-      </div>
+      <SVGWrap>{RightSVGArrows}</SVGWrap>
     </>
   );
 };
